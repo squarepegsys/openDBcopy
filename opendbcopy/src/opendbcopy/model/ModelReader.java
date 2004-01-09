22f41d3c6bdd9b4c76aa4a993a3bbdc950264082
iloveopensource <>
2004-01-09 18:23:10 +0000

    updated header

--- opendbcopy/src/opendbcopy/model/ModelReader.java
+++ opendbcopy/src/opendbcopy/model/ModelReader.java
@@ -1,7 +1,27 @@
-/* ----------------------------------------------------------------------------
+/*
+ * Copyright (C) 2003 Anthony Smith
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * ----------------------------------------------------------------------------
  * TITLE $Id$
  * ---------------------------------------------------------------------------
  * $Log$
+ * Revision 1.1  2004/01/09 18:11:52  iloveopensource
+ * first release
+ *
  * --------------------------------------------------------------------------*/
 package opendbcopy.model;
 
